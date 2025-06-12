163da5a325b8f260cf09fbe731acdf899e0fe30a
Substance-live <shpion160806@gmail.com>
2025-06-12 23:51:02 +0300

    base api

--- server/main.py
+++ server/main.py
@@ -1,15 +1,13 @@
 import os
-from typing import List
+from typing import List, TextIO
+import io
 
 from application.model import Model, FileReader, ExcelExporter
 from application.model import IReader
 
-from fastapi import FastAPI, File, UploadFile, Query, BackgroundTasks
+from fastapi import FastAPI, File, UploadFile, Query, BackgroundTasks, HTTPException
 from fastapi.responses import FileResponse
 
-class FastApiReader(IReader):
-    def read_data(self, file: List[File]) -> List[str]:
-        pass
 
 app = FastAPI()
 model = Model(FileReader(), ExcelExporter())
@@ -18,15 +16,35 @@ model = Model(FileReader(), ExcelExporter())
 def root():
     return {"message": "Hello, FastAPI!"}
 
-@app.post("/data")
-def load_data(UploadFile = File(...)):
-    files = ... # Лист путей на файлы
+@app.delete("/data")
+def clear_data():
+    model.clear_data()
+    return {"message": "Data has been deleted successfully"}
 
-    if files:
-        # model.clear_data()
-        for file in files:
-            try:
-                model.load_data(file)
-            except Exception as e:
-                print(e)
+@app.get("/data")
+def get_data():
+    return model.data
+
+@app.post("/data")
+def insert_data():
+    pass
+
+@app.post("/data/file")
+def load_data_from_file(file: UploadFile = File(...)):
+    text_file = io.TextIOWrapper(file.file, encoding="utf-8")
+    try:
+        model.load_data(text_file)
+    except ValueError as e:
+        raise HTTPException(status_code=422, detail=str(e))
+    return model.data
+
+@app.get("/data/file")
+def get_file_from_data(background_tasks: BackgroundTasks):
+    model.export_data("temporary_excel.xlsx")
+    background_tasks.add_task(lambda path: os.remove(path), "temporary_excel.xlsx")
+    return FileResponse(
+        path="temporary_excel.xlsx",
+        media_type="application/vnd.openxmlformats-officedocument.spreadsheetml.sheet",
+        filename="Отчет_инвентаризации.xlsx"
+    )
 
