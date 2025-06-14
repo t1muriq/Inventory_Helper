ad7b55cbd01d2fc2370f0ac410c542188487042a
Substance-live <shpion160806@gmail.com>
2025-06-14 21:25:53 +0300

    reformat master_key on str type

--- server/main.py
+++ server/main.py
@@ -38,14 +38,14 @@ async def lifespan(application: FastAPI):
 
 app = FastAPI(lifespan=lifespan)
 session_data: Dict[str, Dict[str, Model | datetime]] = dict()
-root_key = 1221
+root_key = "1221"
 
 # ----------- decorators -----------
 
-def admin_required(expected_key: int):
+def admin_required(expected_key: str):
     def decorator(func):
         @wraps(func)
-        def wrapper(*args, master_key: int = Header(...), **kwargs):
+        def wrapper(*args, master_key: str = Header(...), **kwargs):
             if master_key != expected_key:
                 raise HTTPException(status_code=403, detail="Forbidden: Invalid master key")
             return func(*args, **kwargs)
@@ -87,17 +87,17 @@ def root():
 
 @app.get("/root/session")
 @admin_required(root_key)
-def get_all_sessions(master_key: int = Header(...)):
+def get_all_sessions(master_key: str = Header(...)):
     return session_data
 
 @app.get("/root/time")
 @admin_required(root_key)
-def get_time_all_sessions(master_key: int = Header(...)):
+def get_time_all_sessions(master_key: str = Header(...)):
     return [f"До конца сессии {session[0]} осталось {timedelta(minutes=5) - (datetime.now() - session[-1]["last_activity"])}" for session in session_data.items()]
 
 @app.delete("/root/close")
 @admin_required(root_key)
-def close_session_root(master_key: int = Header(...), session_id: str = Query(...)):
+def close_session_root(master_key: str = Header(...), session_id: str = Query(...)):
     session_data.pop(session_id, None)
     return {"message": f"Session: {session_id} has been deleted"}
 
